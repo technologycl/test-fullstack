10db3a45df17d7dc53a7ab4df840533223084fc1
technologycl <78218819+technologycl@users.noreply.github.com>
2023-04-20 08:46:43 -0500

    Add test files

---
+++ readme.md
@@ -0,0 +1,58 @@
+# Prueba Fullstack
+
+## 1. Propósito / Purpose
+
+Lo que se busca para esta prueba es visualizar que el desarrollador tenga conocimientos avanzados en
+Angular y Laravel, que cuente con fuertes bases en lógica y que sobre todo este dispuesto a aprender 
+y que siempre quiera generar mayor valor al equipo en donde se encuentra
+
+/ "We are looking for with this test is to find out that the developer has advanced knowledge in Angular and Laravel.
+Moreover, that the developer has a strong foundation in logic, is willing to learn 
+and that he/she always wants to generate more value to the team where he/she works."
+
+
+## 2. Requerimientos / Requirements
+
+- Fuertes conocimientos en Angular / "Strong knowledge of Angular"
+- Fuertes conocimientos en Javascript / "Strong knowledge of Javascript"
+- Conocimientos en el patrón Repository / "Knowledge in the Repository pattern"
+- Conocimientos en el patrón Container / "Knowledge of the Container pattern" 
+- Fuertes conocimientos en lenguaje PHP / "Strong knowledge of PHP language"
+- Fuertes conocimientos en framework Laravel / "Strong knowledge of Laravel framework"
+- Fuertes conocimientos en el manejo de Git / "Strong knowledge of Git"
+
+
+## Pasos a Seguir / Steps to be followed
+
+1. Debes compartir tu pantalla y activar la cámara durante toda la prueba / "You must share your screen and activate the camera during the entire test."
+2. Lee cuidadosamente las instrucciones de la prueba / "Read the test instructions carefully"
+3. inicia con la prueba de angular (**## 3. Prueba Angular**), lee cuidadosamente los problemas a solucionar,
+ descarga este repositorio corrige el código de acuerdo a lo que se pide en el paso 3 y posterior realiza
+ el paso (**## 3.1 Pasos post prueba**) / "Begin with the angular test (**## 3. Angular test**), read carefully the problems to be fixed,
+ download this repository, correct the code as requested in step 3 and then the step (**## 3.1 Post test steps**)."
+4. Lee atentamente las preguntas y problemas del paso (**## 4. Prueba laravel**) y respóndelas al entrevistador / "Read the questions and problems carefully in the step (**## 4. Test laravel**), and answer them to the interviewer."
+
+## 3. Prueba Angular / Angular Test
+
+Responder las preguntas disponibles en el archivo **test-angular.ts**, mientras se tiene la sesión en vivo con el entrevistador. 
+Esta prueba contiene 3 preguntas que requieren de análisis para poder implementar la mejor solución al caso planteado. / "Answer the questions provided in the **test-angular.ts** file, while having the live session with the interviewer. 
+This test contains 3 questions that require analysis, in order for you to implement the best solution to the case posed."
+
+**Se requiere el desarrollo de código para lograr implementar las soluciones**
+/ **Code development is required to implement the solutions.**
+
+## 3.1 Pasos post prueba / Post-test steps
+
+Ya resueltas las preguntas de la prueba, hacer un fork de este repositorio y compartirlo con la siguiente cuenta de correo: technology@claroinsurance.com / "Once the test questions are solved, make a fork of this repository and share it with the following email account: technology@claroinsurance.com"
+
+
+## 4. Prueba laravel / Test laravel
+
+Responder las preguntas disponibles en el archivo **test-laravel.php**, mientras se tiene la sesión en vivo con el entrevistador.
+ Esta prueba contiene 3 preguntas que requieren de análisis para evaluar la solución planteada y en caso de no ser correcta, 
+ plantear una nueva / "Answer the questions provided in the **test-laravel.php** file, while having the live session with the interviewer.
+ This test contains 3 questions that require analysis to evaluate the solution and in case it is not correct, 
+ you must pose a new one"
+
+**Esta prueba no requiere la modificación o refactorización de su contenido**
+/ **This test does not require modification or refactoring of its content.**
---
+++ test-angular.ts
@@ -0,0 +1,162 @@
+
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, catchError, map, of, tap, retry } from 'rxjs';
+
+
+/**
+ * -- Primer Problema: / First Problem:
+ *
+ * Mostrar el contenido de los observables disponibles en el servicio ContentService
+ * en el template del componente AppComponent. Adicionalmente:
+ *  Title debe estar todo en mayúsculas
+ *  Body debe estar todo en minúsculas
+ *  Footer debe estar capitalizado
+ * /
+ * "Show the content of the observables available in the ContentService 
+ * service in the AppComponent component template. Additionally:"
+ *  Title must be all uppercase
+ *  Body must be all lowercase
+ *  Footer must be capitalized
+ * 
+ * -- Segundo Problema: / Second Problem
+ *
+ * Se necesita que cuando el usuario haga click en el botón "Change Title", asociado al componente ChangeContentComponent,
+ * actualize el titulo ("title") que se ve en el componente AppComponent 
+ * / 
+ * "When the user clicks on the "Change Title" button, associated to the ChangeContentComponent, it is required to, 
+ * update the title ("title") seen in the AppComponent."
+ *
+ *
+ * -- Tercer Problema / Third Problem
+ *
+ * Se necesita que cuando el usuario haga click en el botón "Call Api" asociado al componente ChangeContentComponent
+ * haga el llamado a la api manejada por el servicio GetContentService. En caso de que la conexión sea exitosa debe
+ * mostrarse el mensaje "Connection to API Success", caso contrario debe mostrarse el mensaje "Connection to API Failed"
+ * en el template asociado al componente ChangeContentComponent 
+ * /
+ * "* When the user clicks on the "Call Api" button associated to the ChangeContentComponent , it is necessary to call
+ *  the api operated by the GetContentService. In case the connection is successful the message "Connection to API Success"
+ *  should be displayed, otherwise the message "Connection to API Failed" should be displayed in the template associated 
+ *  to the ChangeContentComponent."
+ */
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <div class="container">
+      <section>
+        <div class="card">
+          <div class="card-title">{{ title }}</div>
+          <br />
+          <hr />
+          <br />
+
+          <div class="card-body">{{ body }}</div>
+          <br />
+          <hr />
+          <br />
+
+          <div class="card-footer">{{ footer }}</div>
+        </div>
+      </section>
+      <change-content></change-content>
+    </div>
+  `,
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title: any;
+  body: any;
+  footer: any;
+
+  constructor() {}
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ContentService {
+  private title: BehaviorSubject<string> = new BehaviorSubject(
+    'Este es el titulo'
+  );
+  private body: BehaviorSubject<string> = new BehaviorSubject(
+    'Este es el contenido principal de la pagina'
+  );
+  private footer: BehaviorSubject<string> = new BehaviorSubject(
+    'Este es el footer '
+  );
+
+  private title$ = this.title.asObservable();
+  private body$ = this.body.asObservable();
+  private footer$ = this.footer.asObservable();
+
+  constructor() {}
+
+  getTitle() {
+    return this.title$;
+  }
+
+  getBody() {
+    return this.body$;
+  }
+
+  getFooter() {
+    return this.footer$;
+  }
+
+  changeTitle() {
+    // Desarrollar el cuerpo del método / Develop the method body
+  }
+}
+
+@Component({
+  selector: 'change-content',
+  template: `
+    <section>
+      <div class="card">
+        <button> Change Title </button>
+        <button> Call Api </button>
+        <section> Connection to API Success  </section>
+        <section> Connection to API Failed  </section>
+      </div>
+    </section>
+  `,
+})
+export class ChangeContentComponent {
+  newTitle = 'Este es el titulo modificado por otro componente';
+  hasError = false;
+  hasContent = false;
+
+  constructor( private service: GetContentService) {}
+
+  changeTitle() {
+    // Desarrollar el cuerpo del método / Develop the method body
+  }
+
+  callApi(){
+    // Desarrollar el cuerpo del método / Develop the method body
+  }
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GetContentService {
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  getContent(){
+    const url = 'https://sitenotexist.com/content/0';
+
+    return this.http.get(url).pipe(
+      retry(2),
+      map( () => 'Respuesta modificada por el servicio'),
+      catchError( (error) => of(error)),
+      tap((value) => console.log('check for value', value)),
+      map( () => ['Respuesta modificada por el servicio nuevamente']),
+    );
+  }
+}
